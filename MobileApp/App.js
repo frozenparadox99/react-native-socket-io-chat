23182d5aa45b4cf850abac4ad1fff810a3ce5e71
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-07-17 15:44:25 +0530

    connected to app

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,8 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import io from "socket.io-client";
 
 export default function App() {
+  useEffect(function () {
+    io("http://192.168.1.20:3001");
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -14,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
 const io = require("socket.io")();
 
+io.on("connection", function () {
+  console.log("a user connected");
+});
+
 io.listen(3001);
