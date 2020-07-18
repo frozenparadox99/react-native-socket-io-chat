8b525ec781e47116824d65db12143e52d598e33a
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-07-18 16:04:26 +0530

    added react navigation

--- MobileApp/App.js
+++ MobileApp/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 
 import HomeScreen from "./screens/HomeScreen";
+import AppContainer from "./AppContainer";
 
 export default function App() {
-  return <HomeScreen />;
+  return <AppContainer />;
 }
